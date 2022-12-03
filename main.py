651a1ceb3c899fe25ddba54ee9ac27a569b29d53
Alexander-Zadorozhnyy <sasha.zadorojnii2013@yandex.ru>
2022-12-03 17:22:16 +0300

    Fixed bug with mask extension.

--- main.py
+++ main.py
@@ -220,9 +220,12 @@ def create_review():
     if request.method == 'POST':
         try:
             file = request.files["photo"]
-            name = f'saved_images/{str(secure_filename(file.filename))}'
-            path = url_for('static', filename=f'saved_images/{str(secure_filename(file.filename))}')
-            converted = corvert_image(file, file.mimetype)
+            filename = '.'.join(str(secure_filename(file.filename)).split('.')[:-1] + ['png'])
+            name = f'saved_images/{filename}'
+            print(name)
+            path = url_for('static', filename=f'saved_images/{filename}')
+            print(path)
+            converted = corvert_image(file)
             converted.save(os.getcwd() + path)
 
             new_review = Review(name=request.form["name"],
--- utils.py
+++ utils.py
@@ -4,14 +4,13 @@ from PIL import Image, ImageOps
 from flask import url_for
 
 
-def corvert_image(image, mimetype):
+def corvert_image(image):
     img = Image.open(image.stream)
     mask = Image.open(os.getcwd() + url_for('static', filename="src/mask.png")).convert('L')
-
+    print(mask)
     output = ImageOps.fit(img, mask.size, centering=(0.5, 0.5))
 
-    if mimetype != 'image/png':
-        output = output.convert('RGB')
+    output = output.convert('RGB')
 
     output.putalpha(mask)
     return output
